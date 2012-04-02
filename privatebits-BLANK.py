a6b7c51fb323d3e0f92b7da66ebd52d3a781a7d6
psychemedia <tony.hirst@gmail.com>
2012-04-02 13:19:23 +0100

    example file for api keys

---
+++ privatebits-BLANK.py
@@ -0,0 +1,42 @@
+def getBitlyKey():
+  bu=''
+  bkey=''
+  return bu,bkey
+
+def getKloutKey():
+  key=''
+  return key
+  
+def getPeerIndexKey():
+  key=''
+  return key
+  
+def getTwapperkeeperKey():
+  key=''
+  return key
+
+def getYahooOAuthKey():
+  key=''
+  shared_secret=''
+  return key,shared_secret
+
+def getYahooAppID():
+  appid=''
+  return appid
+  
+def getTwitterKeys():
+  consumer_key=''
+  consumer_secret=''
+  skey=''
+  ssecret=''  
+  return consumer_key,consumer_secret,skey,ssecret
+
+def getBackTypeKey():
+  key=''
+  return key
+  
+def getLinkedInKey():
+  api_key=''
+  secret_key=''
+  ajhAuth='' #https://github.com/mrgaaron/LinkedIn-Client-Library from liclient import LinkedInAPI
+  return api_key,secret_key
